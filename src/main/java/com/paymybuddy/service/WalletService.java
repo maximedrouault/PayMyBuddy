45043ea19bd390faff820088072b87d9dc192da1
Maxime DROUAULT <119673669+maximedrouault@users.noreply.github.com>
2024-03-15 13:29:31 +0100

    Add WalletService

---
+++ src/main/java/com/paymybuddy/service/WalletService.java
@@ -0,0 +1,18 @@
+package com.paymybuddy.service;
+
+import com.paymybuddy.model.Wallet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WalletService {
+
+    public void withdrawMoney(Wallet wallet, double amountToWithdraw) {
+        wallet.setBalance(wallet.getBalance() - amountToWithdraw);
+    }
+
+    public void addMoney(Wallet wallet, double amountToAdd) {
+        wallet.setBalance(wallet.getBalance() + amountToAdd);
+    }
+}
